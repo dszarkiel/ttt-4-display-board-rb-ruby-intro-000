23daa65002dd591751c1731f2ed2409f905e50d2
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-08-23 18:56:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,3 +6,12 @@ def display_board(board)
   puts "-----------"
   puts "   |   |   "
 end
+
+board = [" "," "," "," ","X"," "," "," "," ",]
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts "   |   |   "
+end
